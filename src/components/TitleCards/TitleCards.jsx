b58722f47e41a0cbc6b7388e60aada8a9e60dc14
Micheal Oshogwe <kenneth.oshogwe@gmail.com>
2024-05-20 18:26:47 +0100

    created the hero and movie card UI

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,32 @@
-import React from "react";
-import "./TitleCards.css";
+import React, { useEffect, useRef } from "react";
+import "./TitleCard.css";
+import cards_data from "../../assets/cards/Cards_data";
 
-const TitleCards = () => {
-  return <div className="titlecards">TitleCards</div>;
+const TitleCards = ({ title, category }) => {
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardRef.current.scrollLeft += event.deltaY;
+  };
+  useEffect(() => {
+    cardRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
+  const cardRef = useRef();
+  return (
+    <div className="title-cards">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TitleCards;
